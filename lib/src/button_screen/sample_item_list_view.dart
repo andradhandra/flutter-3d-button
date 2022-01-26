d9da5570144d5cd0191c889df946561a0357ec98
unknown <andradhandra@gmail.com>
2022-01-26 15:58:06 +0700

    add button

--- lib/src/button_screen/sample_item_list_view.dart
+++ lib/src/button_screen/sample_item_list_view.dart
@@ -4,16 +4,12 @@ import '../settings/settings_view.dart';
 import 'sample_item.dart';
 import 'sample_item_details_view.dart';
 
-/// Displays a list of SampleItems.
 class SampleItemListView extends StatelessWidget {
   const SampleItemListView({
     Key? key,
-    this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
   }) : super(key: key);
 
   static const routeName = '/';
-
-  final List<SampleItem> items;
 
   @override
   Widget build(BuildContext context) {
@@ -39,32 +35,24 @@ class SampleItemListView extends StatelessWidget {
       // In contrast to the default ListView constructor, which requires
       // building all Widgets up front, the ListView.builder constructor lazily
       // builds Widgets as they’re scrolled into view.
-      body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
-        restorationId: 'sampleItemListView',
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          final item = items[index];
-
-          return ListTile(
-            title: Text('SampleItem ${item.id}'),
-            leading: const CircleAvatar(
-              // Display the Flutter Logo image asset.
-              foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+      body: Center(
+        child: Container(
+          height: 50,
+          width: 250,
+          child: const Text(
+            "Mohon Ditekan",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
             ),
-            onTap: () {
-              // Navigate to the details page. If the user leaves and returns to
-              // the app after it has been killed while running in the
-              // background, the navigation stack is restored.
-              Navigator.restorablePushNamed(
-                context,
-                SampleItemDetailsView.routeName,
-              );
-            }
-          );
-        },
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.redAccent,
+              borderRadius: const BorderRadius.all(Radius.circular(24)),
+              border: Border.all(color: Colors.white),
+              boxShadow: [BoxShadow()]),
+        ),
       ),
     );
   }
